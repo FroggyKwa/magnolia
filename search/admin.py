8bc7c32670ddcb41aed7d93d88f4716bfff49fbc
FroggyKwa <ilya.laguxa@gmail.com>
2024-05-25 22:26:41 +1000

    register models to admin panel

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Department)
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+    
