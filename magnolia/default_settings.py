f0a3071278879fb9d13ccbabe2117bcd94a00e9f
FroggyKwa <ilya.laguxa@gmail.com>
2024-05-28 04:26:44 +1000

    remove settings.py from tree

--- magnolia/default_settings.py
+++ magnolia/default_settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = True
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT') if os.getenv('EMAIL_PORT').isdigit() else None)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT')) if os.getenv('EMAIL_PORT').isdigit() else None
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
