599b103f6c4f4f2502e9b7a083933e7247be5678
FroggyKwa <ilya.laguxa@gmail.com>
2024-05-29 04:20:00 +1000

    delete default vue stuff and add PrimeVue UI framework

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+smth
 </template>
+<script setup lang="ts"></script>
+
+<style lang="stylus">
+
+</style>
---
+++ frontend/src/views/LoginVue.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "LoginVue"
+})
+</script>
+
+<template>
+smth
+</template>
+
+<style scoped lang="stylus">
+
+</style>
