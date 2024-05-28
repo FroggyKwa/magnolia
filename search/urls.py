3f4e99b5c3264f0cfd0b44db27fa75b069a5b938
FroggyKwa <ilya.laguxa@gmail.com>
2024-05-29 02:44:02 +1000

    remove unnecessary comments

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from search import views
 
 urlpatterns = [
-    # path("building/", views.BuildingViewSet.as_view(), name="building"),
-    # path("department/", views.DepartmentViewSet.as_view(), name="department"),
-    # path("teacher/", views.TeacherViewSet.as_view(), name="teacher"),
+
 ]
