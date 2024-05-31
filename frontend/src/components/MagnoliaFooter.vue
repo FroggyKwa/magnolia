cd666a65b4d29a0c3d8644c0c1b957c85632d3e9
FroggyKwa <ilya.laguxa@gmail.com>
2024-06-01 01:40:39 +1000

    add footer component

--- frontend/src/components/MagnoliaFooter.vue
+++ frontend/src/components/MagnoliaFooter.vue
@@ -1,10 +1,14 @@
+<template>
+  <div class="w-screen h-5rem surface-100 flex flex-row fixed bottom-0 align-items-center justify-content-center">
+    <div class="text-center ">
+      <p>© Magnolia <span id="year">{{ new Date().getFullYear() }}</span></p>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 
 </script>
-
-<template>
-
-</template>
 
 <style scoped lang="stylus">
 
