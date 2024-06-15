f78ac7b29c6623f3a3adaa34e18d74a35232638e
FroggyKwa <ilya.laguxa@gmail.com>
2024-06-16 04:49:04 +1000

    maybe undefined argument

--- frontend/src/utils/string_utils.ts
+++ frontend/src/utils/string_utils.ts
@@ -1,5 +1,4 @@
-export default function capitalize(str: string) {
+export default function capitalize(str: string | undefined) {
     if (str && str.length)
         return str[0].toUpperCase() + str.slice(1);
-
 }
